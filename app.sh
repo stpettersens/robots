0e09e695b29425c50319cf0731894053c0d0c2e3
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-07-20 16:36:22 +0100

    +correction.

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Run the application outside of Docker container.
 # Uses daemonize: http://software.clapper.org/daemonize
-daemonize -c $(pwd) .$(pwd)/core/robots
+daemonize -c $(pwd) $(pwd)/core/robots
 daemonize -c $(pwd) /usr/bin/node app.js
